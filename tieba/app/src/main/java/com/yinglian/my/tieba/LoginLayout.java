27d57b4a69ab73c360a7411e32c4bcb759031396
lieying.lie@foxmail.com <lieying.lie@foxmail.com>
2018-02-22 16:53:54 +0800

    cookie获取

--- tieba/app/src/main/java/com/yinglian/my/tieba/LoginLayout.java
+++ tieba/app/src/main/java/com/yinglian/my/tieba/LoginLayout.java
@@ -8,6 +8,12 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.IOException;
+import java.util.Map;
+import java.util.List;
+import java.net.MalformedURLException;
 
 public class LoginLayout extends Fragment
 {
@@ -26,13 +32,30 @@ public class LoginLayout extends Fragment
 		webView.canGoForward();
 		webView.setWebViewClient(new WebViewClient(){
 				@Override
-				public boolean shouldOverrideUrlLoading(WebView view, String url)
+				public boolean shouldOverrideUrlLoading(WebView view, String url1)
 				{  
-					if (url.contains("wap.baidu.com"))
+					if (url1.contains("wap.baidu.com"))
 					{
-						
+						try
+						{
+							URL url = new URL(url1);
+							HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+							conn.setRequestMethod("GET");
+							conn.connect();
+							if (conn.getResponseCode() == HttpURLConnection.HTTP_OK)
+							{
+								Map<String, List<String>> headFields = conn.getHeaderFields();
+								List<String> cookieList = headFields.get("Set-Cookie");
+
+							}
+						}
+						catch (MalformedURLException e)
+						{}
+						catch (IOException e)
+						{}
+
 					}
-					view.loadUrl(url);
+					view.loadUrl(url1);
 					return true;  
 				}  
 			});
